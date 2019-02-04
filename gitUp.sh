09eccd330193b8bcbad46d1d3de69b2347a12888
m0e <supamoe25@gmail.com>
2019-02-04 23:57:19 +0100

    update autocommit

--- gitUp.sh
+++ gitUp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git add . ; git commit -a -m "${1:-update autocommit}" ; git push -u ${2:-origin} ${3:-master}
+git add . ; git commit -a -m "${3:-update autocommit}" ; git push -u ${1:-origin} ${2:-master}
